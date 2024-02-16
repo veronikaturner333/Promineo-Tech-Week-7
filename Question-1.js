c7f8f78d0087d98b669f0420a603971c1327b755
Veronika Turner <turner.veronika3@gmail.com>
2024-02-15 23:37:50 -0700

    minor changes

--- Question-1.js
+++ Question-1.js
@@ -3,6 +3,8 @@
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 //Programmatically subtract the value of the first element in the
 //array from the value in the last element of the array
+//declare variable for result which is the last element in the array minus the first
+//Last element is accessed by subtracting one from the length of the array
 let result = ages[ages.length-1]-ages[0];
 //Expected output: 90
 console.log(result);
@@ -13,7 +15,7 @@ console.log(ages);
 
 
 //Use a loop to iterate through the array and calculate the average age
-//Declare variable for sum, "i" assumes value of each element in the array
+//Declare variable for sum which starts at zero, "i" assumes value of each element in the array
 //We keep adding together elements in the array until we get through it (ages.length)
 let sum = 0;
 for (let i = 0; i < ages.length; i++){
